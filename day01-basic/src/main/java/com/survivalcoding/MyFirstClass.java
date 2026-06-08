7272957493bcb93dca0f1b280d78801d001e14d0
Jaewon.Moon <mjw9313@gmail.com>
2026-06-08 10:33:10 +0900

    String 연습

--- day01-basic/src/main/java/com/survivalcoding/MyFirstClass.java
+++ day01-basic/src/main/java/com/survivalcoding/MyFirstClass.java
@@ -6,6 +6,8 @@ public class MyFirstClass {
         // Java 에서 final 을 쓰면 상수를 의미한다.
         // final int i = 0;
 
+        String name = "홍길동";
+
         // sout 치면 아래 코드 자동 완성
         System.out.println("Hello");
     }
