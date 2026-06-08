ca0e901ab3d350a9528e0a65fdc7a4d3f13a9980
Seungbin.Park <waterdog0601@naver.com>
2026-06-08 14:54:44 +0900

    연습문제2

--- game/src/main/java/com/survivalcoding/Cleric.java
+++ game/src/main/java/com/survivalcoding/Cleric.java
@@ -6,4 +6,18 @@ public class Cleric {
     int hp, mp;
     final int maxHp = 50;
     final int maxMp = 10;
+
+    void selfAid() {
+        System.out.println("셀프 에이드");
+        if (mp >= 5) {
+            mp -= 5;
+            hp += 5;
+            if (hp > maxHp) {
+                hp = maxHp;
+            }
+        } else {
+            System.out.println("마나가 부족합니다");
+            return;
+        }
+    }
 }
