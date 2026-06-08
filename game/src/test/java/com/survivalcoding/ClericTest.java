b71b2bbed7d299b5e8503e54324857ccd1a32f04
Yun WooSang <ssaqwe123@gmail.com>
2026-06-08 13:32:03 +0900

    2026-06-08 과제 제출 검증과정 추가

--- game/src/test/java/com/survivalcoding/ClericTest.java
+++ game/src/test/java/com/survivalcoding/ClericTest.java
@@ -18,6 +18,8 @@ class ClericTest {
 
         // when 실행
         test1.selfAid();
+
+        // then 검증
         assertEquals(5, test1.mp);
         assertEquals(50, test1.hp);
     }
@@ -27,15 +29,36 @@ class ClericTest {
     void pray() {
         //given 준비
         Cleric test2 = new Cleric();
+        int tmp;    // 관측용 변수 생성
 
         // 초기값
-        test2.mp = 7;
-        // 관측용 변수
-        int tmp;
+        test2.mp = 5;
 
         // when 실행
-        tmp = test2.pray(1);
-        System.out.println(tmp);
+        tmp = test2.pray(0);
 
+        // then 검증
+        // 검증시 랜덤으로 인해 assertEquals 사용을 못함
+        // 5회 반복하여 실제로 랜덤으로 작동하는지 확인
+        System.out.println("회복량 : " + tmp);
+
+        // 아래는 검증 5회 반복임
+        for (int i = 0; i < 5; i++) {
+            test2.mp = 5;
+            tmp = test2.pray(0);
+            System.out.println("회복량 : " + tmp);
+        }
+
+
+        // 초기값 9로 maxMP를 넘기는 상황을 연축
+        test2.mp = 9;
+
+        // when 실행
+        // 무조건 maxMP를 넘기므로 1이 출력되도록 실행
+        tmp = test2.pray(3);
+
+        // then 검증
+        // maxMP의 값이 10이기에 로직상 tmp는 1이 담겨있어야함.
+        System.out.println("회복량 pray(3) = " + tmp);
     }
 }
