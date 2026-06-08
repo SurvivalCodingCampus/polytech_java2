e8ccee5b297d6e4ef485e4b02f11ffad0bcf0dbc
Junsuk Oh <junsuk.oh@survivalcoding.com>
2026-06-08 10:30:06 +0900

    Java 기본기 연습

---
+++ day01-basic/src/main/java/com/survivalcoding/MyFirstClass.java
@@ -0,0 +1,12 @@
+package com.survivalcoding;
+
+public class MyFirstClass {
+    // main
+    public static void main(String[] args) {
+        int i = 0;
+        i = 10;
+
+        // sout
+        System.out.println("헬로!!!!!!!!!!!!");
+    }
+}
