cea14b22cdeb00f78ad003e7ab7667c0f23d891a
InJoon.Lee <eadel0521@gmail.com>
2026-06-08 11:53:44 +0900

    game

---
+++ game/src/main/java/com/survivalcoding/Cleric.java
@@ -0,0 +1,13 @@
+package com.survivalcoding;
+
+public class Cleric {
+    String name;
+    int hp;
+    final int MAX_HP = 50;
+    int mp;
+    final int MAX_MP = 10;
+    
+    static void main(String[] args) {
+
+    }
+}
