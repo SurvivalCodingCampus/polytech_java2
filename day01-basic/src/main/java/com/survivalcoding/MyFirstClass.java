3b9678b9ad135724b84f613ffabc903e67d1598f
jongjae0305 <jongjae0305@gmail.com>
2026-06-08 10:32:57 +0900

    String practice

---
+++ day01-basic/src/main/java/com/survivalcoding/MyFirstClass.java
@@ -0,0 +1,12 @@
+package com.survivalcoding;
+
+public class MyFirstClass {
+    public static void main(String[] args) {
+        int i = 0;
+        i = 10;
+
+        String name = "홍길동";
+
+        System.out.println("헬로!!!!");
+    }
+}
