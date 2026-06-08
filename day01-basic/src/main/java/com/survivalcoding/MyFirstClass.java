88c837f213906e67565d0b65f549ca6caefd54cb
Yun WooSang <ssaqwe123@gmail.com>
2026-06-08 10:30:11 +0900

    Java 기본기 연습

---
+++ day01-basic/src/main/java/com/survivalcoding/MyFirstClass.java
@@ -0,0 +1,12 @@
+package com.survivalcoding;
+
+public class MyFirstClass {
+    // main -> Enter -> 자동완성
+    public static void main(String[] args) {
+        final int i = 0;    // C에서의 const와 같음 (상수)
+        // sout -> Enter -> 자동완성
+        System.out.println("Hello");
+
+
+    }
+}
