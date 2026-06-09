c177ed6dc216877df9f96b20dd090f6f1256c6b8
Yun WooSang <ssaqwe123@gmail.com>
2026-06-09 10:23:56 +0900

    2026-06-09 피드백 수정 1. 매직넘버제거 2. selfAid()에서 mp 부족할 경우 추가 3. selfAid() EdgeCase 추가

--- game/src/main/java/com/survivalcoding/Cleric.java
+++ game/src/main/java/com/survivalcoding/Cleric.java
@@ -3,25 +3,35 @@ package com.survivalcoding;
 import java.util.Random;
 
 public class Cleric {
-    String name;
-    final int maxHP = 50;
-    int hp;
     final int maxMP = 10;
+    final int maxHP = 50;
+    final int selfAidMPCost = 5;
+    final int randBoundary = 3;
+    final Random rand = new Random();
+
+    String name;
+    int hp;
     int mp = maxMP;
-    
+
     void selfAid() {
-        mp -= 5;
+        // 5 미만일 때 실패하는 로직 구현
+        if (mp < selfAidMPCost) {
+            System.out.println("MP가 부족합니다.");
+            return;
+        }
+        mp -= selfAidMPCost;
         hp = maxHP;
     }
 
     int pray(int second) {
-        Random rand = new Random();
         int healMP = second;
-        healMP += rand.nextInt(3);
+        healMP += rand.nextInt(randBoundary);
         // 최대 MP보다 더 회복 할 수 없음
-        if ((healMP + mp) > maxMP) {
-            healMP = maxMP - mp;
-        }
+        healMP = Math.min(maxMP - mp, healMP);
+//        if ((healMP + mp) > maxMP) {
+//            healMP = maxMP - mp;
+//        }
+        mp += healMP;
         return healMP;
     }
 }
--- game/src/test/java/com/survivalcoding/ClericTest.java
+++ game/src/test/java/com/survivalcoding/ClericTest.java
@@ -3,49 +3,81 @@ package com.survivalcoding;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 class ClericTest {
 
     @Test
     @DisplayName("selfAid 테스트, MP 5 소모 후 HP 회복")
     void selfAid() {
+        // 초기값 준비
+        final int initSelfAidHP = 10;
+        final int initSelfAidMP = 10;
+        final int mpEdgeCase = 4;
+
         // given 준비
         Cleric test1 = new Cleric();
-        // 초기값 준비
-        test1.hp = 10;
-        test1.mp = 10;
+        test1.hp = initSelfAidHP;
+        test1.mp = initSelfAidMP;
 
         // when 실행
         test1.selfAid();
 
         // then 검증
-        assertEquals(5, test1.mp);
-        assertEquals(50, test1.hp);
+        assertEquals(initSelfAidMP - test1.selfAidMPCost, test1.mp);
+        assertEquals(test1.maxHP, test1.hp);
+
+        // mp가 부족할 경우 테스트
+        // given 준비
+        test1.mp = mpEdgeCase;
+        test1.hp = initSelfAidHP;
+
+        // when 실행
+        test1.selfAid();
+
+        // then 검증
+        assertNotEquals(test1.maxHP, test1.hp);
+        assertEquals(mpEdgeCase, test1.mp);
     }
 
     @Test
-    @DisplayName("pray N초 후 N + (0 ~ 2) 만큼 MP 회복")
+    @DisplayName("pray testSecond초 후 testSecond + (0 ~ 2) 만큼 MP 회복")
     void pray() {
+        // 초기값 준비
+        final int initPrayMP = 5;
+        final int overflowInitPrayMP = 9;
+        final int randTestSecond = 0;
+        final int overflowTestSec = 3;
+
         //given 준비
         Cleric test2 = new Cleric();
         int tmp;    // 관측용 변수 생성
 
         // 초기값
-        test2.mp = 5;
+        test2.mp = initPrayMP;
 
         // when 실행
-        tmp = test2.pray(0);
+        tmp = test2.pray(randTestSecond);
 
         // then 검증
         // 피드백 이후 assert 관련 함수들을 찾아서 랜덤값 작동 확인 방법 개선
-        // pray(int second)는 second + (0 ~ 2) 이기 때문에, 회복량은 0 ~ 2 가 되어야 함.
-        assertTrue(tmp >= 0 && tmp <= 2);
-        System.out.println("회복량 : " + tmp);
+        // pray(int second)는 second + (0 ~ 2) 이기 때문에, 회복량은 (현재MP ~ 현재MP + 2) 가 되어야 함.
+        assertTrue(tmp >= initPrayMP && tmp <= (initPrayMP + 2));
 
+        // 초기값 9로 maxMP를 넘기는 상황을 연축
+        test2.mp = overflowInitPrayMP;
+
+        // when 실행
+        // 무조건 maxMP를 넘기므로 1이 출력되도록 실행
+        tmp = test2.pray(overflowTestSec);
+
+        // then 검증
+        // maxMP의 값이 10이기에 로직상 tmp에는 (최대MP - 현재MP)의 값이 담겨있어야함.
+        final int correctAnswer = test2.maxMP - test2.mp;
+        assertEquals(correctAnswer, tmp);
 
         /*
+        // 피드백 수정 전
         // 검증시 랜덤으로 인해 assertEquals 사용을 못함
         // 5회 반복하여 실제로 랜덤으로 작동하는지 확인
         System.out.println("회복량 : " + tmp);
@@ -58,15 +90,5 @@ class ClericTest {
         }
 
         */
-        // 초기값 9로 maxMP를 넘기는 상황을 연축
-        test2.mp = 9;
-
-        // when 실행
-        // 무조건 maxMP를 넘기므로 1이 출력되도록 실행
-        tmp = test2.pray(3);
-
-        // then 검증
-        // maxMP의 값이 10이기에 로직상 tmp는 1이 담겨있어야함.
-        System.out.println("회복량 pray(3) = " + tmp);
     }
 }
