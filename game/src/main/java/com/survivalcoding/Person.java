306e9710e3224cf2a181bbe52777dd7c0b7782fc
MaybeLemon <maybelemon@naver.com>
2026-06-15 18:01:34 +0900

    260615_연습문제2_컬렉션

--- game/src/main/java/com/survivalcoding/Person.java
+++ game/src/main/java/com/survivalcoding/Person.java
@@ -1,26 +1,38 @@
 package com.survivalcoding;
 
-import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Person {
     private final String name;
-    private final int birthYear;
+//    private final int birthYear;
 
-    public Person(String name, int birthYear) {
+    public Person(String name) {
         this.name = name;
-        this.birthYear = birthYear;
+//        this.birthYear = birthYear;
     }
 
     public String getName() {
         return name;
     }
 
-    public int getBirthYear() {
-        return birthYear;
-    }
+//    public int getBirthYear() {
+//        return birthYear;
+//    }
+//
+//    public int getAge() {
+//        int age = LocalDate.now().getYear();
+//        return age - this.birthYear;
+//    }
 
-    public int getAge() {
-        int age = LocalDate.now().getYear();
-        return age - this.birthYear;
+    List<Person> people = new ArrayList<>();
+
+    public void setPeople() {
+        people.add(new Person("홍길동"));
+        people.add(new Person("한석봉"));
+
+        for (Person person : people) {
+            System.out.println(person.getName());
+        }
     }
 }
--- game/src/test/java/com/survivalcoding/PersonTest.java
+++ game/src/test/java/com/survivalcoding/PersonTest.java
@@ -12,19 +12,41 @@ class PersonTest {
     @Test
     @DisplayName("Name")
     void getNameBirthYear() {
-        Person person = new Person("테스트", 2000);
+        Person person = new Person("테스트");
 
         assertEquals("테스트", person.getName());
-        assertEquals(2000, person.getBirthYear());
+//        assertEquals(2000, person.getBirthYear());
     }
 
     @Test
+    @DisplayName("Age")
     void getAge() {
-        Person person = new Person("테스트", 2000);
+        Person person = new Person("테스트");
 
-        int birthYear = person.getBirthYear();
+//        int birthYear = person.getBirthYear();
         int year = LocalDate.now().getYear();
-        int age = year - birthYear;
-        assertEquals(age, person.getAge());
+//        int age = year - birthYear;
+//        assertEquals(age, person.getAge());
+    }
+
+
+    @Test
+    @DisplayName("Person Name")
+    void getName() {
+        Person person = new Person("홍길동");
+
+        assertEquals("홍길동", person.getName());
+    }
+
+    @Test
+    @DisplayName("List에 홍길동과 한석봉을 담는다")
+    void setPeople() {
+        Person person = new Person("테스트용");
+
+        person.setPeople();
+
+        assertEquals(2, person.people.size());
+        assertEquals("홍길동", person.people.get(0).getName());
+        assertEquals("한석봉", person.people.get(1).getName());
     }
 }
