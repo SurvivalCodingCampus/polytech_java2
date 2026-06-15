802c0374d7954d0fbcc6547923db787c66a4641e
MaybeLemon <maybelemon@naver.com>
2026-06-15 15:51:07 +0900

    260615_연습문제 1-1

--- game/src/main/java/com/survivalcoding/Hero.java
+++ game/src/main/java/com/survivalcoding/Hero.java
@@ -1,10 +1,18 @@
 package com.survivalcoding;
 
 public class Hero {
-    String name;
-    int hp;
+    private String name;
+    private int hp;
 
     void attack() {
         System.out.println("공격했다.");
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
     }
 }
