2263fc3d0e2969c678da637371db819d40ab7116
Sunjin.Kim <ks42ks@naver.com>
2026-06-16 11:25:11 +0900

    삭제

--- game2/src/test/java/com/survivalcoding/HeroTest.java
+++ game2/src/test/java/com/survivalcoding/HeroTest.java
@@ -1,22 +1,4 @@
 package com.survivalcoding;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 class HeroTest {
-
-    @Test
-    @DisplayName("공격하면 내 hp가 줄어든다")
-    void attack() {
-        Hero hero = new Hero();
-
-        hero.getHp();
-        hero.attack();
-        hero.attack();
-
-        assertEquals(8, hero.getHp());
-    }
-
 }
