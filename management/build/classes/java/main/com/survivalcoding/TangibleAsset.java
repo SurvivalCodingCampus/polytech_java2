d79714b1b8b7d49e7c9ca6f91e880de8dd1d7379
Sungyeon.Park <sypdana@gmail.com>
2026-06-22 19:11:53 +0900

    260622_추상화와 인터페이스 연습문제1

---
+++ management/build/classes/java/main/com/survivalcoding/TangibleAsset.java
@@ -0,0 +1,18 @@
+public class TangibleAsset extends Asset {
+    private String color;
+
+    //생성자 ..상속포함
+    TangibleAsset(String name, int price, String color) {
+        super(name, price);
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+}
