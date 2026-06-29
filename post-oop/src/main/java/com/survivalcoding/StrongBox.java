8f1baa9fc454fd174d3ebd11050bcf4cae2a4325
Sunjin.Kim <ks42ks@naver.com>
2026-06-29 16:30:42 +0900

    수정

--- post-oop/src/main/java/com/survivalcoding/StrongBox.java
+++ post-oop/src/main/java/com/survivalcoding/StrongBox.java
@@ -1,9 +1,12 @@
 package com.survivalcoding;
 
+import static com.survivalcoding.KeyType.*;
+
 public class StrongBox<UNKNOWN> {
     private UNKNOWN data;
     private int count;
-    private final KeyType keyType;
+    private KeyType keyType;
+
 
     public StrongBox(KeyType keyType) {
         this.keyType = keyType;
@@ -17,11 +20,20 @@ public class StrongBox<UNKNOWN> {
     public UNKNOWN get() {
         count++;
 
-        if (count <= keyType.getDurability()) {
+        if (keyType == PADLOCK && count <= 1024) {
             return null;
         }
-
-        return data;
+        if (keyType == BUTTON && count <= 10000) {
+            return null;
+        }
+        if (keyType == DIAL && count <= 30000) {
+            return null;
+        }
+        if (keyType == FINGER && count <= 1000000) {
+            return null;
+        }
+        return this.data;
     }
 }
+
 
