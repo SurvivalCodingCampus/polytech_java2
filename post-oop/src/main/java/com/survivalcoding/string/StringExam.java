1c4705ee2f977ff4ce8452ed04399424a2b9fc12
Junsuk Oh <junsuk.oh@survivalcoding.com>
2026-07-07 08:37:46 +0900

    문자열 수업

---
+++ post-oop/src/main/java/com/survivalcoding/string/StringExam.java
@@ -0,0 +1,39 @@
+package com.survivalcoding.string;
+
+public class StringExam {
+    public static void main(String[] args) {
+        stringBuilderAdd();
+    }
+
+    public static void stringBuilderAdd() {
+        StringBuilder sb = new StringBuilder("!");
+
+        // text 의 값이 바뀐거냐.
+        sb.append("hello");
+
+
+        // 100만번 : 180ms
+        for (int i = 0; i < 1000000; i++) {
+            sb.append("!");
+        }
+
+        System.out.println(sb.toString().length());  // 10000
+    }
+
+    public static void stringAdd() {
+        String text = "!";
+
+        // text 의 값이 바뀐거냐.
+        text = new String(text + new String("hello"));
+
+
+        // 1만번 : 159ms
+        // 10만번 : 490ms
+        // 100만번 : 23초
+        for (int i = 0; i < 1000000; i++) {
+            text += "!";
+        }
+
+        System.out.println(text.length());  // 10000
+    }
+}
