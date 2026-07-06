c00842336f8cbfa547c8696481fafb08ead50b8d
Yungchang Cho <dudckd2411@gmail.com>
2026-07-06 17:42:18 +0900

    0706 퀴즈2

--- Game/src/main/java/com/survivalcoding/Word.java
+++ Game/src/main/java/com/survivalcoding/Word.java
@@ -15,4 +15,11 @@ public class Word {
                 || letters.substring(i, i + 1).equals("u");
     }
 
+    public boolean isConsonant(int i) {
+        return !letters.substring(i, i + 1).equals("a")
+                && !letters.substring(i, i + 1).equals("e")
+                && !letters.substring(i, i + 1).equals("i")
+                && !letters.substring(i, i + 1).equals("o")
+                && !letters.substring(i, i + 1).equals("u");
+    }
 }
