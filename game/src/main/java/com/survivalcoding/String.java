8e712ee2b977d1ca3d6239e683e250e02ab300e0
SuHyun <mneo1213@naver.com>
2026-07-06 16:26:58 +0900

    260706_18_서수현-2

--- game/src/main/java/com/survivalcoding/String.java
+++ game/src/main/java/com/survivalcoding/String.java
@@ -1,4 +1,21 @@
 package com.survivalcoding;
 
 public class String {
+    java.lang.String num = "0";
+
+    void string() {
+
+        StringBuilder num = new StringBuilder();
+
+        for (int i = 1; i < 101; i++) {
+            num.append(i);
+            num.append(",");
+        }
+
+        String[] a = num.split("[,]");
+        for (String word : a) {
+            System.out.println(word + "->");
+        }
+
+    }
 }
