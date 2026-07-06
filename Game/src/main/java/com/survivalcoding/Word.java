e6c8057ed2c0e824cf635d5e5f523d58db0537ea
Yungchang Cho <dudckd2411@gmail.com>
2026-07-06 17:42:10 +0900

    0706 퀴즈1

---
+++ Game/src/main/java/com/survivalcoding/Word.java
@@ -0,0 +1,18 @@
+package com.survivalcoding;
+
+public class Word {
+    private String letters;
+
+    public Word(String letters) {
+        this.letters = letters;
+    }
+
+    public boolean isVowel(int i) {
+        return letters.substring(i, i + 1).equals("a")
+                || letters.substring(i, i + 1).equals("e")
+                || letters.substring(i, i + 1).equals("i")
+                || letters.substring(i, i + 1).equals("o")
+                || letters.substring(i, i + 1).equals("u");
+    }
+
+}
